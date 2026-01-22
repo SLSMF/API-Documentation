f3879208af173fa29d1dde3f78f52524886587b4
Dias Bakeev <dias.bakeev@vliz.be>
2026-01-22 15:43:41 +0100

    update github documentation
    examples

--- Examples/garc/R/R_code.R
+++ Examples/garc/R/R_code.R
@@ -16,21 +16,25 @@ rm(list = ls())
 api_key <- '' # enter API key
 station_ID <- 'garc'
 sensor <- 'one-sensor'
-days_per_page <- 360  # maximum number of days spanning a page, max 3650 days
+days_per_page <- 360  # maximum number of days spanning a page, max 365 days
 start_date <- '2023-01-01'  # from date, included in result. Default is 8days in the past, 'YYYY-MM-DD'
 end_date <- '2023-02-01'  # until date, not included in result. Default is day of request 'YYYY-MM-DD'
 includesensors <- 'rad'
 level_data <- 'true' # Level data relative to the mean sea level of 30 days.
-original_stime <- 'false' # Return the stime not corrected by sensor rate.
-filter_out_of_range <- 'true' # Remove out of range values
-filter_exceeded_neighbours <- 'true' # Remove exceeded neighbours values
-filter_spikes_via_median <- 'true' # Remove spikes via median values
-filter_flat_line <- 'true' # Remove flat line
+flag_qc <- 'true' # show qc flags
+fit_sample_interval <- 'true' # Return the stime not corrected by sensor rate.
+filter_out_of_range <- 'true' # Set slevel to NA for records with out of range values\n,
+filter_exceeded_neighbours <- 'true' # Set slevel to NA for records with exceeded neighbour values\n,
+filter_spikes_via_median <- 'true' # Set slevel to NA for records with spikes via median values\n,
+filter_flat_line <- 'true' # Set slevel to NA for records with flat lines\n,
+filter_completeness <- 'false' # Set slevel to NA for days with low completeness\n,
+filter_distinctness <- 'false' # Set slevel to NA for days with low distinctness\n,
+filter_shift <- 'false' # Set slevel to NA for days with shift\n
 
 ##########################################################################
 # download and read research-quality sea level data
 
-api_url <- 'https://api.ioc-sealevelmonitoring.org/v2/research/stations/'
+api_research_data_url <- 'https://api.ioc-sealevelmonitoring.org/v2/research/stations/'
 
 # Calculate number of days and pages
 no_of_days <- as.Date(end_date) - as.Date(start_date)
@@ -42,42 +46,50 @@ S <- data.frame()
 # Loop to download data
 for (i in 1:no_of_pages) {
   page <- i  # current page number requested
-  
+
   # Building the custom URL
   if (nchar(includesensors) > 0) {
-    url <- paste0(api_url, station_ID, '/sensors/', sensor, 
+    url <- paste0(api_research_data_url, station_ID, '/sensors/', sensor,
                   '/data?days_per_page=', days_per_page,
-                  '&page=', page, 
-                  '&timestart=', start_date, 
-                  '&timestop=', end_date, 
-                  '&includesensors[]=', includesensors, 
-                  '&level_data=', level_data, 
-                  '&original_stime=', original_stime, 
-                  '&filter_out_of_range=', filter_out_of_range, 
+                  '&page=', page,
+                  '&timestart=', start_date,
+                  '&timestop=', end_date,
+                  '&includesensors[]=', includesensors,
+                  '&level_data=', level_data,
+                  '&flag_qc=', flag_qc,
+                  '&fit_sample_interval=', fit_sample_interval,
+                  '&filter_out_of_range=', filter_out_of_range,
                   '&filter_exceeded_neighbours=', filter_exceeded_neighbours,
-                  '&filter_spikes_via_median=', filter_spikes_via_median, 
-                  '&filter_flat_line=', filter_flat_line)
+                  '&filter_spikes_via_median=', filter_spikes_via_median,
+                  '&filter_flat_line=', filter_flat_line,
+                  '&filter_completeness=', filter_completeness,
+                  '&filter_distinctness=', filter_distinctness,
+                  '&filter_shift=', filter_shift)
   } else {
-    url <- paste0(api_url, station_ID, '/sensors/', sensor, 
+    url <- paste0(api_research_data_url, station_ID, '/sensors/', sensor,
                   '/data?days_per_page=', days_per_page,
-                  '&page=', page, 
-                  '&timestart=', start_date, 
-                  '&timestop=', end_date, 
-                  '&level_data=', level_data, 
-                  '&original_stime=', original_stime, 
-                  '&filter_out_of_range=', filter_out_of_range, 
+                  '&page=', page,
+                  '&timestart=', start_date,
+                  '&timestop=', end_date,
+                  '&level_data=', level_data,
+                  '&flag_qc=', flag_qc,
+                  '&fit_sample_interval=', fit_sample_interval,
+                  '&filter_out_of_range=', filter_out_of_range,
                   '&filter_exceeded_neighbours=', filter_exceeded_neighbours,
-                  '&filter_spikes_via_median=', filter_spikes_via_median, 
-                  '&filter_flat_line=', filter_flat_line)
+                  '&filter_spikes_via_median=', filter_spikes_via_median,
+                  '&filter_flat_line=', filter_flat_line,
+                  '&filter_completeness=', filter_completeness,
+                  '&filter_distinctness=', filter_distinctness,
+                  '&filter_shift=', filter_shift)
   }
-  
+
   # Downloading the data
-  message(paste('Downloading page', page, '/', no_of_pages, 'of the data requested'))
+  message(paste('Downloading page', page, '/', no_of_pages, 'of the v2 API data requested'))
   response <- GET(url, add_headers(`X-Api-Key` = api_key, Accept = 'text/csv'))
-  
+
   # Writing the output file(s)
   data.table::fwrite(list(content(response, "text")), file = paste0('SLSMF_tg_data_pg', page, '.txt'), quote=F)
-  
+
   # Read the tables back
   if (i == 1) {
     S <- read_tsv(paste0('SLSMF_tg_data_pg', page, '.txt'), skip = 2)
@@ -88,30 +100,33 @@ for (i in 1:no_of_pages) {
 }
 
 ##########################################################################
-# Download v1 API data without post-processing
+# Download v2 API data without post-processing
+api_realtime_data_url <- "https://api.ioc-sealevelmonitoring.org/v2/stations/"
 Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 30)
-frm <- 'json' # format of the output file (max 30 days)
-v1_api_url <- 'https://ioc-sealevelmonitoring.org/service.php?query=data'
 
 # Building the custom URL
 if (nchar(includesensors) > 0) {
-  url_link <- paste0(v1_api_url, '&code=',station_ID, 
-                     '&timestart=', start_date,'T00%3A00', 
-                     '&timestop=', end_date,'T00%3A00',
-                     '&format=', frm, 
-                     '&includesensors[]=', includesensors) 
+  url_link <- paste0(api_realtime_data_url, station_ID,
+                     '/data?days_per_page=', days_per_page,
+                     '&timestart=', start_date,'T00:00',
+                     '&timestop=', end_date,'T00:00',
+                     '&includesensors[]=', includesensors)
 } else {
-  url_link <- paste0(v1_api_url, '&code=',station_ID, 
-                     '&timestart=', start_date,'T00%3A00', 
-                     '&timestop=', end_date,'T00%3A00',
-                     '&format=', frm) 
+  url_link <- paste0(api_realtime_data_url, station_ID,
+                     '/data?days_per_page=', days_per_page,
+                     '&timestart=', start_date,'T00:00',
+                     '&timestop=', end_date,'T00:00')
 }
-# Downloading the data
-message(paste('Downloading v1 API data requested'))
-tmp <- read_json(url_link,simplifyVector = TRUE)
 
-# Process the table
-Spre <- data.frame(stime = strptime(tmp$stime,format="%Y-%m-%d %H:%M:%S", tz = "GMT"), slevel = tmp[1])
+# Downloading the data
+message(paste('Downloading page', page, '/', no_of_pages, 'of the v2 API data requested'))
+response <- GET(url_link, add_headers(`X-Api-Key` = api_key, Accept = 'application/json'))
+response <- content(response, as = "text", encoding = "UTF-8")
+tmp <- fromJSON(response, simplifyVector = TRUE)
+colnames(tmp) = gsub("data\\.", "", colnames(tmp))
+tmp$stime <- as.POSIXct(tmp$stime,format="%Y-%m-%d %H:%M:%S",tz="UTC")
+Spre = tmp
+
 ##########################################################################
 # plot the data
 
